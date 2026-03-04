a65bc206a00df87de6483e0f726ead2e0aae6bae
Giovanni Rella <giomrella@gmail.com>
2026-03-04 12:08:57 -0500

    removing tailscale

--- home.nix
+++ home.nix
@@ -53,7 +53,6 @@
     xclip
     nixd
     openssh
-    tailscale
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
