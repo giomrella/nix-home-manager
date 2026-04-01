a6cd084e6b16a0accdaad2b07edcd0917f081e71
Giovanni Rella <giomrella@gmail.com>
2026-04-01 10:31:38 -0400

    adding x2goclient

--- home.nix
+++ home.nix
@@ -53,6 +53,7 @@
     xclip
     nixd
     openssh
+    x2goclient
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
